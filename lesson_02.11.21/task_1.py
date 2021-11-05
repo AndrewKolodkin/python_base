ba8f1a9d204fb04ac9301c4c98c55ef36753ab63
Andrew Kolodkin <Koloandrew2012@gmail.com>
2021-11-05 16:49:03 +0500

    Great function ver_1

---
+++ lesson_02.11.21/task_1.py
@@ -0,0 +1,8 @@
+def num_translation(args):
+    dict_words = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
+                  'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять'}
+    print ('Введите цифру от 0 до 9 для перевода')
+    for keys in dict_words:
+     if dict_words.setdefault(key, default=):
+         return
+num_translation(zero)
