9f9e9670818963b4503f0ed587a587b4c384c174
Andrew Kolodkin <Koloandrew2012@gmail.com>
2021-11-08 21:52:05 +0500

    Great function final

--- task_1.py
+++
@@ -1,8 +0,0 @@
-def num_translation(words):
-    dict_words = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
-                  'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
-
-    return dict_words.get(words, 'None')
-
-
-print(num_translation('zero'))
