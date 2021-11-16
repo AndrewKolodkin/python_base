9b644d614de2b40d90e889f8a195f17ee58f1d5d
Andrew Kolodkin <Koloandrew2012@gmail.com>
2021-11-16 20:04:44 +0500

    task_1 final

---
+++ lesson_09.11.2021/task_1.py
@@ -0,0 +1,26 @@
+def odd_nums(n):
+    for num in range(1, n + 1):
+        if num % 2 > 0:
+            yield num
+
+
+odd_to_15 = odd_nums(15)
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print('-' * 50)
+n = int(input('n = : '))
+odd_to_15 = (num for num in range(1, n + 1) if num % 2 > 0)
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
