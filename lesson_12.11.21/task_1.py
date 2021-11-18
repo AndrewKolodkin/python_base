71b96d8b528a450dc32ea74701888ac721c45334
Andrew Kolodkin <Koloandrew2012@gmail.com>
2021-11-18 22:59:48 +0500

    solution to task_1 rev.1

--- lesson_12.11.21/task_1.py
+++ lesson_12.11.21/task_1.py
@@ -8,7 +8,10 @@ with open('nginx_logs.txt', 'r+') as f:
     for f_log in url.content.decode(encoding='UTF-8'):
         f.write(f_log)
     for line in f:
-        print(line)
-#         ln = line.split()
-#         res.append(ln[0], ln[3].strip('"'), ln[6])
-# print(res)
+        # print(line)
+        ln = line.split()
+        # print(ln)
+        res.append(ln[0])
+        res.append(ln[5].strip('"'))
+        res.append(ln[5])
+print(res)
