049aff8c92c7b8a022d536306d9d9e336da0a941
Andrew Kolodkin <Koloandrew2012@gmail.com>
2021-11-19 19:31:39 +0500

    solution to task_1 rev.1

--- lesson_12.11.21/task_1.py
+++ lesson_12.11.21/task_1.py
@@ -1,17 +1,11 @@
-import requests
-
-url = requests.get(
-    'https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs')
 res = []
-
-with open('nginx_logs.txt', 'r+') as f:
-    for f_log in url.content.decode(encoding='UTF-8'):
-        f.write(f_log)
+with open('nginx_logs.txt', 'r', decode='utf-8') as f:
     for line in f:
-        # print(line)
+        print(line)
         ln = line.split()
         print(ln)
+
 #         res.append(ln[0])
 #         res.append(ln[5].strip('"'))
-#         res.append(ln[5])
+#         res.append(ln[6])
 # print(res)
