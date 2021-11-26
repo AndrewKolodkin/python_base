4045fa1de785f2876e7dc3a4b8570e43bb167fc0
Andrew_Kolodkin <Koloandrew2012@gmail.com>
2021-11-26 14:10:02 +0500

    Task_1 finall

---
+++ lesson_19.11.21/task_1.py
@@ -0,0 +1,14 @@
+import re
+
+
+def email_parse(email_address):
+    pattern = re.compile(r'^(?P<username>\w+)@(?P<domain>\w+\.\w+)$')
+
+    if re.match(pattern, email_address) is not None:
+        print('Проверка пройдена')
+    else:
+        raise ValueError(f'Error, {email_address} not valid')
+    return email_address
+
+
+print(email_parse('Koloandrew2012@gmail.com'))
